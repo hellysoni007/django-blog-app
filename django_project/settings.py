cb6d0a59109672f8f9d077e6ff64856aabd70ca5
Helly Soni <helly.inexture@gmail.com>
2022-05-23 15:49:01 +0530

    change for static files

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,14 +123,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media')
 )
 
 
