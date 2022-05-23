5e6d2ddafe22f6006e3b5c43959d0f3c0c6600b5
Helly Soni <helly.inexture@gmail.com>
2022-05-23 18:49:09 +0530

    Update gitignore

--- django_project/settings.py
+++ django_project/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_project',
+        'USER': 'postgres',
+        'PASSWORD': 'Helly@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
